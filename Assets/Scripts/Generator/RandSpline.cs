7ff9f4b3b8004d917cb40af226151a0410a223be
ViggyNash <viggyviggynash@gmail.com>
2016-09-27 19:51:43 -0400

    WHY THE FUCK DOES IT NOT WORK

--- Assets/Scripts/Generator/RandSpline.cs
+++ Assets/Scripts/Generator/RandSpline.cs
@@ -2,9 +2,11 @@
 using System.Collections;
 using System;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class RandSpline : MonoBehaviour {
 
     private BezierSpline spline;
+    private MeshFilter mesh;
 
     public bool randomSegmentNum;
     public float segments;
@@ -25,6 +27,8 @@ public class RandSpline : MonoBehaviour {
     // Use this for initialization
     void Awake()
     {
+        mesh = GetComponent<MeshFilter>();
+
         if (!allowHeight)
         {
             heightScaling = 0;
@@ -98,8 +102,11 @@ public class RandSpline : MonoBehaviour {
         {
             stepSize = 1f / (stepSize - 1);
         }
+
+        vertices = new Vector3[3 * (frequency)];
+        triangles = new int[12 * (frequency - 1)];
         points = new Vector3[frequency];
-        Segment[] segments = new Segment[frequency - 1];
+        Segment[] segments = new Segment[frequency];
         Segment newSegment;
         for (int f = 0; f < frequency - 1; f++)
         {
@@ -109,41 +116,118 @@ public class RandSpline : MonoBehaviour {
             newSegment = new Segment(points[f], points[f + 1]
                                     , spline.GetVelocity(f * stepSize), spline.GetVelocity((f + 1) * stepSize)
                                     , segmentWidth);
+            segments[f] = newSegment;
 
             //GameObject loc = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Cube));
             //loc.transform.position = position;
         }
 
-        //newSegment = new Segment(points[frequency], points[0]);
+        initMeshData(segments);
+        Mesh newMesh = new Mesh();
+        newMesh.name = "Road Mesh";
+        newMesh.vertices = vertices;
+        for (int i = 0; i < triangles.Length; i++)
+        {
+            Debug.Log(i % 12 + " " + triangles[i]);
+            if (i % 12 == 11)
+            {
+                Debug.Log(" ");
+            }
+        }
+
+        Debug.Log(vertices.Length);
+        newMesh.triangles = triangles;
+
+        mesh.mesh = newMesh;
+    }
+
+    //Populates "vertices" and "triangles" arrays to prepare for sending to Mesh API
+    private void initMeshData(Segment[] segments){
+        vertices[0] = segments[0].orthogonalStartPlus;
+        vertices[1] = segments[0].start;
+        vertices[2] = segments[0].orthogonalStartMinus;
+
+        int length = segments.Length - 2;
+        int j;
+        for (int i = 0; i < length; i++)
+        {
+            j = ((i) * 3);
+
+            //Debug.Log(i * 3 + " " + vertices.Length);
+            vertices[3 + j] = segments[i].orthogonalEndPlus;
+            vertices[4 + j] = segments[i].end;
+            vertices[5 + j] = segments[i].orthogonalEndMinus;
+
+            j = ((i + 1) * 3);
+
+            triangles[i * 12] = j - 3;
+            triangles[1 + (i * 12)] = j - 2;
+            triangles[2 + (i * 12)] = j;
+            triangles[3 + (i * 12)] = j - 2;
+            triangles[4 + (i * 12)] = j;
+            triangles[5 + (i * 12)] = j + 1;
+            triangles[6 + (i * 12)] = j - 2;
+            triangles[7 + (i * 12)] = j + 1;
+            triangles[8 + (i * 12)] = j + 2;
+            triangles[9 + (i * 12)] = j - 2;
+            triangles[10 + (i * 12)] = j - 1;
+            triangles[11 + (i * 12)] = j + 2;
+        }
+
+        j = vertices.Length - 3;
+
+        triangles[length * 12] =  - 3;
+        triangles[1 + (length * 12)] = j - 2;
+        triangles[2 + (length * 12)] = j;
+        triangles[3 + (length * 12)] = j - 2;
+        triangles[4 + (length * 12)] = j;
+        triangles[5 + (length * 12)] = j + 1;
+        triangles[6 + (length * 12)] = j - 2;
+        triangles[7 + (length * 12)] = j + 1;
+        triangles[8 + (length * 12)] = j + 2;
+        triangles[9 + (length * 12)] = j - 2;
+        triangles[10 + (length * 12)] = j - 1;
+        triangles[11 + (length * 12)] = j + 2;
+
     }
 
     private class Segment
     {
-        Triangle[] triangles;
-        Vector3 orthogonalStart;
-        Vector3 orthogonalEnd;
+        public Triangle[] triangles;
+        public Vector3 start;
+        public Vector3 end;
+        public Vector3 orthogonalStartPlus;
+        public Vector3 orthogonalEndPlus;
+        public Vector3 orthogonalStartMinus;
+        public Vector3 orthogonalEndMinus;
 
         //Creates a 4 triangle, 6 point segment
         public Segment(Vector3 start, Vector3 end, Vector3 vectorStart, Vector3 vectorEnd, float segmentWidth)
         {
             triangles = new Triangle[4];
 
-            start = Vector3.ProjectOnPlane(start, Vector3.up);
-            end = Vector3.ProjectOnPlane(end, Vector3.up);
+            this.start = start;
+            this.end = end;
 
-            orthogonalStart = Vector3.Cross(vectorStart, Vector3.up).normalized * segmentWidth;
-            orthogonalEnd = Vector3.Cross(vectorEnd, Vector3.up).normalized * segmentWidth;
+            vectorStart = Vector3.ProjectOnPlane(vectorStart, Vector3.up);
+            vectorEnd = Vector3.ProjectOnPlane(vectorEnd, Vector3.up);
 
-            triangles[0] = new Triangle(orthogonalStart, orthogonalEnd, start);
-            triangles[1] = new Triangle(start, orthogonalEnd, end);
-            triangles[2] = new Triangle(start, end, -orthogonalEnd);
-            triangles[3] = new Triangle(start, -orthogonalEnd, -orthogonalStart);
+            this.orthogonalStartPlus = Vector3.Cross(vectorStart, Vector3.up).normalized * segmentWidth;
+            this.orthogonalEndPlus = Vector3.Cross(vectorEnd, Vector3.up).normalized * segmentWidth;
+
+            this.orthogonalStartMinus = Vector3.Cross(Vector3.up, vectorStart).normalized * segmentWidth;
+            this.orthogonalEndMinus = Vector3.Cross(Vector3.up, vectorEnd).normalized * segmentWidth;
+
+            triangles[0] = new Triangle(orthogonalStartPlus, orthogonalEndPlus, start);
+            triangles[1] = new Triangle(start, orthogonalEndPlus, end);
+            triangles[2] = new Triangle(start, end, orthogonalEndMinus);
+            triangles[3] = new Triangle(start, orthogonalEndMinus, orthogonalStartMinus);
         }
     }
 
     private class Triangle
     {
-        Vector3[] vertices = new Vector3[3];
+        public Vector3[] vertices = new Vector3[3];
 
         public Triangle(Vector3 p1, Vector3 p2, Vector3 p3)
         {
